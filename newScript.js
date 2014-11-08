c3a0b7e1d3b06822295ad294c6f1a6585add8390
719Ben <BenWilliams4560@gmail.com>
2014-11-08 16:50:46 -0700

    Updated newScript to get past Facebook

--- newScript.js
+++ newScript.js
@@ -2,18 +2,63 @@
  * Created by Ben on 11/8/14.
  */
 
-var entries = $(".userContent").length;
+
+//get the start item
+var currentItem = $(".userContent")[0];
+
+//when the page loads
+$(document).ready(function () {
+
+    //run main to start
+    main();
+});
+
+//when someone clicks something
+$(document).on("click", function() {
+
+    //wait two seconds
+    window.setTimeout(function () {
+
+        //if the items has changed
+        if ($(".userContent")[0] != currentItem) {
+
+            //debugging log
+            console.log("change url");
+
+            //change the start text
+            currentItem = $(".userContent")[0]
+
+            //run main again
+            main();
+        }
+    }, 2000);
+});
 
 
-for(var i = 0; i < entries; i++) {
+//main function
+function main() {
 
-    var dataDom = $(".userContent").slice(i, i+1);
-    var dataText = dataDom.text();
+    //get the amount of entrees
+    var entries = $(".userContent").length;
 
-    var encyt_flag = dataText.slice(0, 3);
+    //loop through the items
+    for (var i = 0; i < entries; i++) {
 
-    if(encyt_flag == "enc") {
+        //slice up the items
+        var dataDom = $(".userContent").slice(i, i + 1);
+        var dataText = dataDom.text();
 
-        dataDom.text("new message");
+        //check the encryption flag
+        var encyt_flag = dataText.slice(0, 3);
+
+        //debugging log
+        console.log("flag:", encyt_flag);
+
+        //if the flag is valid, change the message
+        if (encyt_flag == "enc") {
+
+            //temp message change
+            dataDom.html("new message");
+        }
     }
-}
+};
