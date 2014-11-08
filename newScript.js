6ffba46ad32f6db9f8ace549f285906dd841aed5
719Ben <BenWilliams4560@gmail.com>
2014-11-08 14:52:31 -0700

    Updated javascript

--- newScript.js
+++ newScript.js
@@ -1,3 +1,18 @@
 /**
  * Created by Ben on 11/8/14.
  */
+
+var entries = $(".userContent").length;
+
+
+for(var i = 0; i < entries; i++) {
+
+    var data = $(".userContent")[i].childNodes[0].childNodes;
+
+    var encyt_flag = data.slice(0, 3);
+
+    if(encyt_flag == "enc") {
+
+        data = "secret message";
+    }
+}
